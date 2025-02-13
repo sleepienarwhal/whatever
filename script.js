4d751d11c3a12dbf654f694ac4441abb887196c3
sleepienarwhal <lamip1304@gmail.com>
2025-02-13 23:55:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,9 +72,9 @@ function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1.5) % messages.length;
+    messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 2}px`;
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
 }
 
 function handleYesClick() {
