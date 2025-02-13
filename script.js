9dac142913e67fe34586d053e8bddb911800d6a4
sleepienarwhal <lamip1304@gmail.com>
2025-02-13 23:54:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Pookie please...",
+    "Pwease...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
@@ -72,9 +72,9 @@ function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
+    messageIndex = (messageIndex + 1.5) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 2}px`;
 }
 
 function handleYesClick() {
